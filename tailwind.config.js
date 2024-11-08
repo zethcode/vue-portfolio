b96f5fc035c07a2a5b4d737f92aa390918165d57
Zeth <arckie.jadulco@gmail.com>
2024-11-08 10:59:48 +0800

    feat: added tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+  ],
+  plugins: [require('flowbite/plugin')],
+  theme: {},
+}
